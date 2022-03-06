7238ab18965ecfbee030e35b46ec3a010254aa4d
xiaoqiang cheng <xiaoqiang.cheng@foxmail.com>
2022-03-06 23:41:07 +0800

    view test

--- view.py
+++ view.py
@@ -1,5 +1,25 @@
-import netron
-import os
+# import netron
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QSplitter, QTreeWidgetItem, QCheckBox
+from PySide2.QtCore import QTimer, Qt, QModelIndex
 
-ret = netron.start("/home/cxq/Develop/lab_code/FastPlugin/plugin.onnx", browse=False)
-print(ret)
+class View():
+    '''
+        mvc模式中的视图部分，本身不包含任何逻辑，仅作界面显示使用
+    '''
+    def __init__(self):
+        self.ui = QUiLoader().load('config/main.ui')
+        # ret = netron.start("/home/cxq/Develop/lab_code/FastPlugin/plugin.onnx", browse=False)
+        # print(ret)
+
+    def show(self):
+        self.ui.show()
+
+if __name__=="__main__":
+    from qt_material import apply_stylesheet
+
+    app = QApplication([])
+    apply_stylesheet(app, theme='dark_teal.xml')
+    obj = View()
+    obj.show()
+    app.exec_()
